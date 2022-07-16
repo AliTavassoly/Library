04255f95eb4240ffc94e80777cf12c1848396f4f
AliTavassoly <tavassolyali@gmail.com>
2022-07-16 15:31:30 +0430

    Add book class

--- src/shared/model/Book.java
+++ src/shared/model/Book.java
@@ -1,4 +1,23 @@
 package shared.model;
 
 public class Book {
+    private String name, author;
+    private int id;
+
+    public Book() {}
+
+    public Book(String name, String author, int id) {
+        this.name = name;
+        this.author = author;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
